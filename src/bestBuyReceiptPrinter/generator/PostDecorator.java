ddcbae3d885050801e50dfe391f11f49836eedbb
rmbackoTU-dev <rmbacko92@gmail.com>
2020-10-19 22:32:08 -0400

    Initial Skeleton

--- src/bestBuyReceiptPrinter/generator/PostDecorator.java
+++ src/bestBuyReceiptPrinter/generator/PostDecorator.java
@@ -3,6 +3,9 @@ package bestBuyReceiptPrinter.generator;
 import java.math.BigDecimal;
 import bestBuyReceiptPrinter.addOns.Addon;
 
+/**
+* Applies any discount addOns to the purchaseItems
+*/
 public class PostDecorator extends ReceiptDecorator {
 
 	private Addon discountAddon;
