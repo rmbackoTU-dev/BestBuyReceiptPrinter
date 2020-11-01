8a5e9de6a48b29328a1d88b62778396e4d8f522f
Ryan Backof <rmbacko92@gmail.com>
2020-10-31 22:31:18 -0400

    Merged addOns branch with Master

--- src/bestBuyReceiptPrinter/generator/ReceiptDecorator.java
+++ src/bestBuyReceiptPrinter/generator/ReceiptDecorator.java
@@ -2,6 +2,8 @@ package bestBuyReceiptPrinter.generator;
 
 public abstract class ReceiptDecorator implements Receipt {
 
+	Receipt currentReceipt;
+	
 	@Override
 	public void printReceipt() {
 		// TODO Auto-generated method stub
